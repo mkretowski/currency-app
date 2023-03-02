8c9816adb4fc8bf41f9786a60d8a7e128eac947c
Mateusz Kretowski <matkretkurs@gmail.com>
2023-03-02 01:47:01 +0100

    Add convertPLNtoUSD function tests

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,11 +1,21 @@
 export const convertPLNToUSD = (PLN) => {
+  if (typeof PLN === 'string' || PLN instanceof String || PLN === undefined) {
+    return NaN;
+  }
+
+  if (typeof PLN != 'string' && typeof PLN != 'number') {
+    return 'Error';
+  }
+
+  if (PLN <= 0) {
+    return '$0.00';
+  }
 
   const PLNtoUSD = PLN / 3.5;
-  
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
-    currency: 'USD'
+    currency: 'USD',
   });
 
   return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
-}
+};
