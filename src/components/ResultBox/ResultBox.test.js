88ab2b8790422266b820502170624460f00c5ca7
Mateusz Kretowski <matkretkurs@gmail.com>
2023-03-05 15:20:47 +0100

    Change outputAmount evaluating method in ResultBox tests

--- src/components/ResultBox/ResultBox.test.js
+++ src/components/ResultBox/ResultBox.test.js
@@ -2,6 +2,8 @@ import ResultBox from './ResultBox';
 import '@testing-library/jest-dom/extend-expect';
 import { render, screen, cleanup } from '@testing-library/react';
 import { formatAmountInCurrency } from '../../utils/formatAmountInCurrency';
+import { convertPLNToUSD } from '../../utils/convertPLNToUSD';
+import { convertUSDToPLN } from '../../utils/convertUSDToPLN';
 
 describe('Component ResultBox', () => {
   it('should render without crashing', () => {
@@ -24,7 +26,7 @@ describe('Component ResultBox', () => {
 
       // check if conversion output is correct
       const inputAmount = formatAmountInCurrency(testObj.amount, testObj.from);
-      const outputAmount = formatAmountInCurrency(testObj.amount / 3.5, testObj.to);
+      const outputAmount = convertPLNToUSD(testObj.amount);
       const resultTxt = inputAmount + ' = ' + outputAmount;
       expect(output).toHaveTextContent(resultTxt);
       // unmount component
@@ -48,7 +50,7 @@ describe('Component ResultBox', () => {
 
       // check if conversion output is correct
       const inputAmount = formatAmountInCurrency(testObj.amount, testObj.from);
-      const outputAmount = formatAmountInCurrency(testObj.amount * 3.5, testObj.to);
+      const outputAmount = convertUSDToPLN(testObj.amount);
       const resultTxt = inputAmount + ' = ' + outputAmount;
       expect(output).toHaveTextContent(resultTxt);
       // unmount component
